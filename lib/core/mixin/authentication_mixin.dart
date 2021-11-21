182a4503b1ddd669645d414ee62b9883f5a910c6
Federico Viceconti <viceconti.federico@gmail.com>
2021-11-21 17:38:57 +0100

    adding check validation token on login and check if token is valid before request

--- lib/core/mixin/authentication_mixin.dart
+++ lib/core/mixin/authentication_mixin.dart
@@ -3,8 +3,10 @@ import 'package:unofficial_twitch_auth/unofficial_twitch_auth.dart';
 mixin AuthenticationMixin {
   Future<bool> validateToken(
     TwitchAuthentication authentication,
-    String token,
+    String? token,
   ) async {
+    if(token == null || token.isEmpty) return false;
+
     final validateResponse = await authentication.validate(
       accessToken: token,
     );
--- lib/functionalities/login_screen/login_screen_viewmodel.dart
+++ lib/functionalities/login_screen/login_screen_viewmodel.dart
@@ -1,12 +1,31 @@
+import 'package:provider/provider.dart';
+import 'package:unofficial_twitch_auth/twitch_authentication.dart';
+import 'package:unofficial_twitch_mobile/core/mixin/authentication_mixin.dart';
 import 'package:unofficial_twitch_mobile/core/navigation/home/route_navigation.dart';
+import 'package:unofficial_twitch_mobile/core/storage/extension/persistent_storage_extension.dart';
 import 'package:unofficial_twitch_mobile/utils/notifier/base_notifier.dart';
 
-class LoginScreenViewModel extends BaseNotifier {
+class LoginScreenViewModel extends BaseNotifier with AuthenticationMixin {
+  final TwitchAuthentication authentication;
+
   LoginScreenViewModel({
     required RouteNavigation navigation,
-  }) : super(navigation: navigation);
+  })  : authentication =
+            Provider.of<TwitchAuthentication>(navigation.navigationContext),
+        super(
+          navigation: navigation,
+        );
 
-  void onTapGoToLogin() {
-    navigation.pushNamed(NameRoute.loginWebView);
+  void onTapGoToLogin() async {
+    final isValidToken = await validateToken(
+      authentication,
+      await persistData.accessToken,
+    );
+
+    if(isValidToken) {
+      //TODO go to home
+    } else {
+      navigation.pushNamed(NameRoute.loginWebView);
+    }
   }
 }
