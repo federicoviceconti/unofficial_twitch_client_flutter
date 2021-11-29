62bec795c72d1f14255de8b2bd6c1d4b724112ca
Federico Viceconti <viceconti.federico@gmail.com>
2021-11-29 09:46:32 +0100

    adding data

---
+++ external_modules/unofficial_twitch_store_local/lib/unofficial_twitch_store_local.dart
@@ -0,0 +1,4 @@
+library unofficial_twitch_store_local;
+
+export 'package:unofficial_twitch_store_local/local_storage/extension/twitch_persistent_storage_extension.dart';
+export 'package:unofficial_twitch_store_local/local_storage/twitch_persistent_storage_data.dart';
---
+++ external_modules/unofficial_twitch_store_local/test/unofficial_twitch_store_local_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:unofficial_twitch_store_local/unofficial_twitch_store_local.dart';
+
+void main() {
+
+}
