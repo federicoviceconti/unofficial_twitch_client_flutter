ffd65bceb3f42d7e244e708154ed02d1a0d1b4df
Federico Viceconti <viceconti.federico@gmail.com>
2021-12-01 11:25:49 +0100

    adding searchStreams on twitch_manager_search_mixin.dart

--- lib/core/mixin/twitch_manager_search_mixin.dart
+++ lib/core/mixin/twitch_manager_search_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:unofficial_twitch_http/models/http_result.dart';
 import 'package:unofficial_twitch_mobile/utils/notifier/base_notifier.dart';
+import 'package:unofficial_twitch_open_api/search/model/open_api_get_streams_response.dart';
 import 'package:unofficial_twitch_open_api/search/model/open_api_search_categories_response.dart';
 import 'package:unofficial_twitch_open_api/search/model/open_api_search_channels_response.dart';
 import 'package:unofficial_twitch_open_api/search/model/open_api_search_users_response.dart';
@@ -27,5 +28,26 @@ mixin TwitchManagerSearchMixin on BaseTwitchManagerMixin, BaseNotifier {
   }) {
     final instance = getApiInstance<TwitchSearch>();
     return instance.searchCategories(query: query);
+  }
+
+  Future<HttpResult<OpenApiGetStreamsResponse>> searchStreams({
+    final String? after,
+    final String? before,
+    final int? first,
+    final String? gameId,
+    final String? language,
+    final String? userId,
+    final String? userLogin,
+  }) async {
+    final instance = getApiInstance<TwitchSearch>();
+    return await instance.searchStreams(
+      after: after,
+      before: before,
+      first: first,
+      gameId: gameId,
+      language: language,
+      userId: userId,
+      userLogin: userLogin,
+    );
   }
 }
